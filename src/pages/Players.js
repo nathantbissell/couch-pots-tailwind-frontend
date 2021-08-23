b67a9d0704cc9e36f93ca609732383082a2c6a95
Nathan Bissell <nathan@syncwithconnex.com>
2021-08-22 20:33:09 -0400

    added test array data

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -21,6 +21,20 @@ export default function Players() {
   const basePlayers = {
     players: BasePlayers,
   };
+
+  const devPlayers = {
+    players: [
+      {
+        name: 'Josh Allen',
+        position: 'QB',
+        auctionPrice: 11,
+        totalPoints: 418.0,
+        average: 26.125,
+        bio: "Allen is the perfect example of why rushing is so important when it comes to QB fantasy evaluations. The 2018 first-round pick finished as fantasy's No. 6 QB last season despite ranking 23rd in passing yards, 21st with only 20 passing TDs, third worst in completion percentage (59%) and 11th worst in YPA (6.7). Of course, Allen also produced a 109-510-9 rushing line and has now paced the position in rushing TDs in each of his two NFL seasons (17 total). Allen's passing troubles remain a red flag, but the addition of Stefon Diggs should help his cause. As long as he keeps running, Allen will be a back-end QB1 option.",
+        otherLeagueDraftValue: 8.93,
+      },
+    ],
+  };
   useEffect(() => {
     async function fetchAxios() {
       const result = await axios(`${localhost}/players`);
